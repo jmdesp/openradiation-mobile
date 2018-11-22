c1fa55ed5561c2913d3e285c33ae65bf151a62c4
Tom Demulier--Chevret <tdemulier@itlink.fr>
2018-11-22 13:10:40 +0100

    Add back unused StorageService methods

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -7,11 +7,27 @@ import { from, Observable } from 'rxjs';
 export class StorageService implements StorageEngine {
   constructor(private storage: Storage) {}
 
+  length(): Observable<number> {
+    return from(this.storage.length());
+  }
+
   getItem(key: any): Observable<any> {
     return from(this.storage.get(key));
   }
 
   setItem(key: any, val: any): void {
     this.storage.set(key, val);
+  }
+
+  removeItem(key: any): void {
+    this.storage.remove(key);
+  }
+
+  clear(): void {
+    this.storage.clear();
+  }
+
+  key(val: number): Observable<string> {
+    return from(this.storage.keys().then(keys => keys[val]));
   }
 }
